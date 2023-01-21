eb2fa401d1159474c80227be8759007f65a375b2
Diego Espinosa <55413117+dieg0espx@users.noreply.github.com>
2023-01-20 18:00:40 -0600

    Initial Commit

---
+++ pruebaIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pruebaIOS
+//
+//  Created by DIEGO ESPINOSA on 2023-01-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
